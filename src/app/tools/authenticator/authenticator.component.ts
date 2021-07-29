d9b8b67c07374eb6e6f9d9628f7b9487ffff1890
TimothyMFelice <timothymfelice@gmail.com>
2021-07-28 20:30:14 -0700

    feat: :sparkles: Added Functions To Create Login and Reset

--- src/app/tools/authenticator/authenticator.component.ts
+++ src/app/tools/authenticator/authenticator.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FirebaseTSAuth } from 'firebasets/firebasetsAuth/firebaseTSAuth'
 
 @Component({
   selector: 'app-authenticator',
@@ -8,9 +9,76 @@ import { Component, OnInit } from '@angular/core';
 export class AuthenticatorComponent implements OnInit {
 
   state = AuthenticatorCompState.LOGIN;
-  constructor() { }
+  firebaseAuth: FirebaseTSAuth;
+
+  constructor() {
+    this.firebaseAuth = new FirebaseTSAuth();
+  }
 
   ngOnInit(): void {
+  }
+
+  onResetClick(resetEmail: HTMLInputElement) {
+    let email = resetEmail.value;
+
+    if (this.isNotEmpty(email)){
+      this.firebaseAuth.sendPasswordResetEmail(
+        {
+        email: email,
+        onComplete: (err) => {
+          alert(`Reset email sent to ${email}`);
+        }
+      });
+    }
+  }
+
+  onLogin(loginEmail: HTMLInputElement, loginPassword: HTMLInputElement) {
+    let email = loginEmail.value;
+    let password = loginPassword.value;
+
+    if (this.isNotEmpty(email) && this.isNotEmpty(password)) {
+      this.firebaseAuth.signInWith(
+        {
+          email: email, 
+          password: password,
+          onComplete: (uc) => {
+            alert("Logged In");
+          },
+          onFail: (err) => {
+            alert(err);
+          }
+        });
+    }
+  }
+
+  onRegisterClick(registerEmail: HTMLInputElement, registerPassword: HTMLInputElement, registerConfirmPassword: HTMLInputElement) {
+    let email = registerEmail.value;
+    let password = registerPassword.value;
+    let confirmPassword = registerConfirmPassword.value;
+
+    if (this.isNotEmpty(email) && this.isNotEmpty(password) && this.isNotEmpty(confirmPassword) && this.isAMatch(password, confirmPassword)) {
+      this.firebaseAuth.createAccountWith({
+        email: email,
+        password: password,
+        onComplete: (uc) => {
+          alert("Account Created!");
+          registerEmail.value = "";
+          registerPassword.value = "";
+          registerConfirmPassword.value = "";
+        },
+        onFail: (err) => {
+          alert("Failed to create the account.");
+        }
+      });
+    }
+  }
+
+  isNotEmpty(text: string) {
+    return text != null && text.length > 0;
+  }
+
+  isAMatch(text: string, comparedWith: string) {
+    return text == comparedWith;
   }
 
   onForgotPasswordClick() {
