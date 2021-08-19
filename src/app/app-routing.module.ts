bcbb6aeb26c133e265e22124112134549014acce
TimothyMFelice <timothymfelice@gmail.com>
2021-08-19 16:27:28 -0700

    SCSS Project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ActivityComponent } from './pages/activity/activity.component';
-import { HomeComponent } from './pages/home/home.component';
-import { LandingComponent } from './pages/landing/landing.component';
 
-const routes: Routes = [
-  { path: "", redirectTo: 'home', pathMatch: 'full' },
-  { path: "home", component: HomeComponent },
-  { path: "landing", component: LandingComponent },
-  { path: "activity", component: ActivityComponent },
-  { path: "**", component: LandingComponent }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,51 +1,16 @@
 import { NgModule } from '@angular/core';
-import { AngularFireModule } from '@angular/fire';
 import { BrowserModule } from '@angular/platform-browser';
-import { environment } from 'src/environments/environment';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import { LandingComponent } from './pages/landing/landing.component';
-import { ActivityComponent } from './pages/activity/activity.component';
-import { RatingComponent } from './tools/rating/rating.component'
-import { HeaderComponent } from './tools/header/header.component';
-import { SideNavComponent } from './tools/side-nav/side-nav.component'
-import { HomeComponent } from './pages/home/home.component'
-
-import { MatCardModule } from '@angular/material/card'
-import { MatFormFieldModule } from '@angular/material/form-field'
-import { MatInputModule } from '@angular/material/input'
-import { MatButtonModule } from '@angular/material/button';
-import { MatDividerModule } from '@angular/material/divider'
-import { MatListModule } from '@angular/material/list';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon'
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LandingComponent,
-    ActivityComponent,
-    RatingComponent,
-    HeaderComponent,
-    SideNavComponent,
-    HomeComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    AngularFireModule.initializeApp(environment.firebaseConfig),
-    BrowserAnimationsModule,
-    MatCardModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatButtonModule,
-    MatDividerModule,
-    MatListModule,
-    MatSidenavModule,
-    MatIconModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
